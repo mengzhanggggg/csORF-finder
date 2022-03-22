865654be7ac1ca3b60a0bcd01f4c1b9e4db3033a
mengzhang95 <mengzhang@nuaa.edu.cn>
2022-03-22 20:13:40 +0800

    my code

---
+++ code/test_csORF-finder.py
@@ -0,0 +1,5 @@
+import csORF_finder
+from csORF_finder import test_model
+
+test_model('D:/csORF-finder_program/input_files/M.musculus/non-CDS/', 'D:/csORF-finder_program/output_files/',
+           'mm_ncrna_test.txt', 'M.musculus', 'non-CDS')
