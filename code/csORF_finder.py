08d136a90924e1629a84c9ad5f26a7c0695ba120
mengzhanggggg <84058941+mengzhanggggg@users.noreply.github.com>
2022-03-28 14:26:01 +0800

    Update csORF_finder.py

--- code/csORF_finder.py
+++ code/csORF_finder.py
@@ -52,21 +52,21 @@ def get_sORF(fastas):
                                 g+=1
                                 sequence_name = '>' + name + '_sORF' + str(g)
                                 sORF_seq.append([sequence_name, sequence])
-                                break
+                            break
                         if seg_end == 'TAG':
                             sequence = seq[j:k+3]
                             if np.mod(len(sequence), 3) == 0 and 12 <= len(sequence) <= 303:
                                 g+=1
                                 sequence_name = '>' + name + '_sORF' + str(g)
                                 sORF_seq.append([sequence_name, sequence])
-                                break
+                            break
                         if seg_end == 'TGA':
                             sequence = seq[j:k+3]
                             if np.mod(len(sequence), 3) == 0 and 12 <= len(sequence) <= 303:
                                 g+=1
                                 sequence_name = '>' + name + '_sORF' + str(g)
                                 sORF_seq.append([sequence_name, sequence])
-                                break
+                            break
         elif len(seq) <= 303 and np.mod(len(seq), 3) != 0:
             for j in range(len(seq)-2):
                 seg_start = seq[j:j+3]
@@ -79,21 +79,21 @@ def get_sORF(fastas):
                                 g+=1
                                 sequence_name = '>' + name + '_sORF' + str(g)
                                 sORF_seq.append([sequence_name, sequence])
-                                break
+                            break
                         if seg_end == 'TAG':
                             sequence = seq[j:k+3]
                             if np.mod(len(sequence), 3) == 0 and 12 <= len(sequence) <= 303:
                                 g+=1
                                 sequence_name = '>' + name + '_sORF' + str(g)
                                 sORF_seq.append([sequence_name, sequence])
-                                break
+                            break
                         if seg_end == 'TGA':
                             sequence = seq[j:k+3]
                             if np.mod(len(sequence), 3) == 0 and 12 <= len(sequence) <= 303:
                                 g+=1
                                 sequence_name = '>' + name + '_sORF' + str(g)
                                 sORF_seq.append([sequence_name, sequence])
-                                break
+                            break
         elif seq[0:3] == 'ATG' and seq[len(seq)-3:len(seq)] == 'TAA' and np.mod(len(seq), 3) == 0 and 12 <= len(seq) <= 303:
             sORF_seq.append([name, seq])
         elif seq[0:3] == 'ATG' and seq[len(seq)-3:len(seq)] == 'TAG' and np.mod(len(seq), 3) == 0 and 12 <= len(seq) <= 303:
